5b1c06fd465fa8a09f53fb8d390b30f70f2e6241
trice-jones <trice.jones@outlook.com>
2022-02-07 21:21:31 -0500

    HW 1

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Firefox(executable_path='./geckodriver.exe')
 driver.maximize_window()
 
 # open the url
